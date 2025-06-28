01ef2b54e8b23f7e1efa3d16e6f7e82b75f90a8f
Zero-iinfinity <machinelearinig.vikas@gmail.com>
2025-06-29 01:14:23 +0530

    added some more code

--- Os_module.py
+++ Os_module.py
@@ -60,3 +60,37 @@ print(os.path.exists(r'/tmp/test.txt'))
 '''splits the path and extension'''
 print(os.path.splitext(r'/tmp/test.txt'))
 
+
+#to append a whole list use .extent
+
+'''spliting filename and extension'''
+# import os
+# array = ['ABCD.txt', 'XYZ.txt']
+# new_array = [os.path.splitext(name)[0] for name in array] # This [0] is for getting 1 item of a list for the example it is ABCD,XYZ
+# #if you use [1] it output .txt,.txt
+# print(new_array)
+
+'''to remove file'''
+# os.remove('test.txt')
+
+'''difference btw exists and isfile'''
+# 1. `os.path.exists()`:
+#     - Checks if a path exists
+#     - Returns for both files AND directories `True`
+#     - Use when you just need to know if something exists, regardless of type
+#
+# 2. `os.path.isfile()`:
+#     - Checks if a path exists AND is a regular file
+#     - Returns ONLY for files `True`
+#     - Returns for directories, symbolic links, etc. `False`
+#     - Use when you specifically need to confirm something is a file
+
+'''moving file from one directory to another directory'''
+# import os
+#
+# # Define the source file path and the destination directory path
+# source_file = 'path/to/source/file.txt'
+# destination_directory = 'path/to/destination/'
+#
+# # Move the file
+# os.rename(source_file, destination_directory + os.path.basename(source_file))
